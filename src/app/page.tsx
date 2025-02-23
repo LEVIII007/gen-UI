cc9e4b5ef671689beb73d613c3359a5361439635
thedemonsid <siddheshshrirame@gmail.com>
2025-02-23 15:59:59 +0530

    add LeftHero component and update Home page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import LeftHero from "@/components/left-hero-component";
+
 export default function Home() {
   return (
- <main>
-  
- </main>
+    <main className="max-w-[1440px] mx-auto flex flex-col md:flex-row space-y-12 md:space-y-0 md:space-x-12 relative pt-24">
+      <div className="w-full md:w-1/2 flex flex-col items-start justify-center md:-mt-12">
+        <LeftHero></LeftHero>
+      </div>
+    </main>
   );
 }
