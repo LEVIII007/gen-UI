37a7f576e860f0ff453bd661499a447c49ea5b32
thedemonsid <siddheshshrirame@gmail.com>
2025-02-25 02:19:08 +0530

    fix: remove unnecessary padding from main container in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import RightHero from "@/components/right-hero-component";
 
 export default function Home() {
   return (
-    <main className="max-w-[1440px] mx-auto flex flex-col md:flex-row space-y-12 md:space-y-0 md:space-x-12 relative pt-24">
+    <main className="max-w-[1440px] mx-auto flex flex-col md:flex-row space-y-12 md:space-y-0 md:space-x-12 relative">
       <div className="w-full md:w-1/2 flex flex-col items-start justify-center md:-mt-12 animate-colorChange">
         <LeftHero />
       </div>
